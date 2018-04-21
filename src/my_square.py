a08c640aaa4e699ec68afe1fbe856c48c765ba95
Teju <pradeep.reddy28@gmail.com>
2018-04-21 15:30:43 -0400

    added a second function for calculationg square

--- src/my_square.py
+++ src/my_square.py
@@ -3,5 +3,8 @@ def my_square(y):
         """Takes a value and returns square of it"""
        """uses ** operator"""
         return(y ** 2)
+def my_square2(x):
+       return(x * x)
 print(my_square(42))
+print(my_square2(42))
 
